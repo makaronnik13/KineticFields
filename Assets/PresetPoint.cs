9e55a888823a0749c3e6477400cfd56d0cb38868
Makaronnik13 <biggreenorange@gmail.com>
2021-02-10 18:11:27 +0300

    PresetsInterpolation

---
+++ Assets/PresetPoint.cs
@@ -0,0 +1,30 @@
+﻿using com.armatur.common.flags;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PresetPoint : MonoBehaviour,  IDragHandler
+{
+    [SerializeField]
+    private PresetSquare Preview;
+
+    public GenericFlag<float> Volume = new GenericFlag<float>("Volume", 0);
+
+    public void Init(KineticPreset preset)
+    {
+        Preview.Init(preset);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, -150f, 150f), Mathf.Clamp(transform.localPosition.y, -150f, 150f), 0);
+    }
+
+    void Update()
+    {
+        float dist = Mathf.Clamp(transform.localPosition.magnitude,0,120f);
+        Volume.SetState(Mathf.Lerp(1,0, dist/120f));
+    }
+}
---
+++ Assets/PresetsLerper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresetsLerper : MonoBehaviour
+{
+    private KineticSession Session;
+
+    [SerializeField]
+    private GameObject View;
+
+    [SerializeField]
+    private GameObject PointPrefab;
+
+    private List<PresetPoint> points = new List<PresetPoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        KineticFieldController.Instance.Session.AddListener(SessionChanged);
+    }
+
+    private void SessionChanged(KineticSession session)
+    {
+        this.Session = session;
+    }
+
+    public void SetState(bool v)
+    {
+        View.SetActive(v);
+        if (v)
+        {
+            int i = 0;
+
+            foreach (KineticPreset preset in KineticFieldController.Instance.Session.Value.Presets)
+            {
+                CreatePoint(preset, 360f/ KineticFieldController.Instance.Session.Value.Presets.Count*i);
+            }
+        }
+        else
+        {
+            foreach (PresetPoint p in points)
+            {
+                Destroy(p.gameObject);
+            }
+            points.Clear();
+        }
+    }
+
+    private void CreatePoint(KineticPreset preset, float angle)
+    {
+        GameObject newPoint = Instantiate(PointPrefab);
+        newPoint.transform.localScale = Vector3.one;
+        newPoint.transform.SetParent(View.transform);
+        newPoint.transform.localPosition = new Vector3(120f*Mathf.Cos(angle*Mathf.Rad2Deg) , 120f * Mathf.Sin(angle * Mathf.Rad2Deg), 0);
+        PresetPoint point = newPoint.GetComponent<PresetPoint>();
+        point.Init(preset);
+        points.Add(point);
+    }
+}
