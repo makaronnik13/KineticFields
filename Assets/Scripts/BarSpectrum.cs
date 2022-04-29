5241526b84555095b4ccb7fdc96dfc6aeb93f827
makaronnik13 <13nik@mail.ru>
2022-04-29 21:19:25 +0300

    start refactoring

--- Assets/Scripts/BarSpectrum.cs
+++ Assets/Scripts/BarSpectrum.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using Assets.WasapiAudio.Scripts.Unity;
 using UnityEngine;
+using Zenject;
 
 namespace Assets.Scripts
 {
-    public class BarSpectrum : AudioVisualizationEffect
+    public class BarSpectrum : MonoBehaviour, ITickable
     {
         private GameObject[] _spectrumBars;
         private Vector3[] _originalPositions;
@@ -13,14 +14,23 @@ namespace Assets.Scripts
         private float[] _spectrumCaps;
         private float[] _spectrumBarsSensivity;
 
+        public AudioVisualizationProfile Profile;
+        public AudioVisualizationStrategy Strategy;
+        public bool Smoothed;
 
+        public int SpectrumSize { get; private set; }
+
+        private FFTSerivce fftService;
 
         public GameObject Prefab;
         public float AudioScale;
         public float Power;
 
-        public void Start()
+
+        public BarSpectrum(FFTSerivce fftService)
         {
+            this.fftService = fftService;
+            SpectrumSize = fftService.SpectrumSize;
             _spectrumBars = new GameObject[SpectrumSize];
             _originalPositions = new Vector3[SpectrumSize];
             _originalScale = Prefab.transform.localScale;
@@ -42,7 +52,13 @@ namespace Assets.Scripts
             _spectrumCaps = GetSpectrumData();
         }
 
-        public void Update()
+
+        public float[] GetSpectrumData()
+        {
+            return fftService.GetSpectrumData(Strategy, Smoothed, Profile);
+        }
+
+        public void Tick()
         {
             var spectrumData = GetSpectrumData();
 
@@ -51,27 +67,27 @@ namespace Assets.Scripts
                 return;
             }
 
-                for (var i = 0; i < SpectrumSize; i++)
+            for (var i = 0; i < SpectrumSize; i++)
+            {
+                if (_spectrumCaps[i] < spectrumData[i])
                 {
-                    if (_spectrumCaps[i] < spectrumData[i])
-                    {
-                        _spectrumCaps[i] = spectrumData[i];
-                    }
-                    else
-                    {
-                        _spectrumCaps[i] -= Time.deltaTime;
-                    }
-                    var audioScale = Mathf.Pow(spectrumData[i] * AudioScale, Power);
-                    var newScale = new Vector3(_originalScale.x, _originalScale.y + audioScale, _originalScale.z);
-                    var halfScale = newScale / 2.0f;
-                    _spectrumBars[i].transform.localPosition = new Vector3(_originalPositions[i].x + halfScale.x, _originalPositions[i].y + halfScale.y, _originalPositions[i].z + halfScale.z);
-                    _spectrumBars[i].transform.localScale = newScale;
-
-                    newScale = new Vector3(_originalScale.x, _originalScale.y + audioScale, _originalScale.z);
-
-                    //_spectrumBars[i].transform.GetChild(0).localPosition = Vector3.zero;
-                    //_spectrumBars[i].transform.GetChild(0).localScale = newScale;
+                    _spectrumCaps[i] = spectrumData[i];
                 }
+                else
+                {
+                    _spectrumCaps[i] -= Time.deltaTime;
+                }
+                var audioScale = Mathf.Pow(spectrumData[i] * AudioScale, Power);
+                var newScale = new Vector3(_originalScale.x, _originalScale.y + audioScale, _originalScale.z);
+                var halfScale = newScale / 2.0f;
+                _spectrumBars[i].transform.localPosition = new Vector3(_originalPositions[i].x + halfScale.x, _originalPositions[i].y + halfScale.y, _originalPositions[i].z + halfScale.z);
+                _spectrumBars[i].transform.localScale = newScale;
+
+                newScale = new Vector3(_originalScale.x, _originalScale.y + audioScale, _originalScale.z);
+
+                //_spectrumBars[i].transform.GetChild(0).localPosition = Vector3.zero;
+                //_spectrumBars[i].transform.GetChild(0).localScale = newScale;
+            }
         }
     }
 }
